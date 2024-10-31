d21aec7854a9627e1cc9a9a29dd902a8ee4e9bc3
tre184 <74012437+tre184@users.noreply.github.com>
2024-10-31 16:03:12 +0100

    UPD 1

--- notebooks/train_model.py
+++ notebooks/train_model.py
@@ -4,6 +4,7 @@ from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import os
 
 def main():
     # Configurer MLflow pour utiliser un chemin relatif dans le dossier du projet
